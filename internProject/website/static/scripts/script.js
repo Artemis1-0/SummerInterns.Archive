721c686b4c566b5b0487441cb5d87ac5a40e1b12
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-13 20:47:08 +0100

    Tried something with css

--- internProject/website/static/scripts/script.js
+++ internProject/website/static/scripts/script.js
@@ -24,3 +24,23 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const panels = gsap.utils.toArray(".panel")
+let tl = gsap.timeline({paused: true})
+tl.to("#text-svg", {scale: 100, xPercent: -300 ,  transformOrigin: "50% 50%"})
+.to("#text-svg", {scale: "+=30", xPercent: "-=110"  })
+
+panels.forEach(panel => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top",
+    end: "90% 30%",
+    scrub: 1,
+    pin: true,
+
+    animation: tl.play()
+  })
+})
