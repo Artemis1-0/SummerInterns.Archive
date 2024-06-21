43004f67f35506b2357cf9d456ed97af66c74652
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-21 09:56:19 +0100

    Test navbar log in system

--- internProject/website/static/scripts/navbar.js
+++ internProject/website/static/scripts/navbar.js
@@ -1,8 +1,10 @@
-function hide() {
+function log_out() {
     document.getElementById('booking').hidden = true
-    document.getElementById('log_in').hidden = true
- }
- function show() {
-    document.getElementById('booking').hidden = false
     document.getElementById('log_in').hidden = false
+    document.getElementById('account').hidden = true
+ }
+ function log_in() {
+    document.getElementById('booking').hidden = false
+    document.getElementById('log_in').hidden = true
+    document.getElementById('account').hidden = false
  }
