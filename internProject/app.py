cb1dc9e73ba6df36762db20e7aa1301d5750dfd4
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-24 13:52:32 +0100

    Added 404

--- internProject/app.py
+++ internProject/app.py
@@ -203,6 +203,9 @@ def delete_booking(booking_id):
 
     return redirect(url_for('account'))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404  
 
 if __name__ == '__main__':
     app.run(debug=True)
