6a4122f52dcceed779dcc82e738eb90fc9a4d98f
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-24 13:50:59 +0100

    404

--- internProject/app.py
+++ internProject/app.py
@@ -155,5 +155,9 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404  
+
 if __name__ == '__main__':
     app.run(debug=True)
