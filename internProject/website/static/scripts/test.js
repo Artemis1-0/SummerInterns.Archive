913d679b4282396c6a7860ec9b40224d043fc507
ejisemm <emmanuel.jiss@ericsson.com>
2024-06-25 15:23:56 +0100

    Test stuff

--- internProject/website/static/scripts/test.js
+++ internProject/website/static/scripts/test.js
@@ -31,3 +31,11 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+}
+else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+}
